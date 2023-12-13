644e12b0a7d9054c7c69cc9632662bb4439027b9
Mykhailo <matsyshynmykhailo@gmail.com>
2023-12-13 02:37:06 +0200

    fixed nearestNeighbor method

--- TravellingSalesmanProblem/nearestNeighbor.c
+++ TravellingSalesmanProblem/nearestNeighbor.c
@@ -2,7 +2,8 @@
 
 #include "graph.h"
 
-void nearestNeighbor(Graph* graph) {
+void nearestNeighbor(Graph* graph) 
+{
     // 1. Початок з випадкової вершини або заданої початкової точки
     int startVertex = 0; // Вибір початкової вершини (може бути будь-якою)
     int currentVertex = startVertex;
@@ -13,9 +14,10 @@ void nearestNeighbor(Graph* graph) {
         visited[i] = 0; // Позначаємо, що жодна вершина не відвідана
     }
 
-    // Змінна для зберігання шляху
+    // Змінна для зберігання шляху та загальної відстані
     int path[MAX_CITIES];
     int pathIndex = 0;
+    double totalDistance = 0.0;
 
     // 2. Обхід вершин
     visited[startVertex] = 1; // Позначаємо початкову вершину як відвідану
@@ -23,23 +25,34 @@ void nearestNeighbor(Graph* graph) {
 
     while (pathIndex < graph->numCities) {
         int nearestVertex = -1; // Найближча вершина
+        double minDistance = -1; // Мінімальна відстань
 
         // Знаходження найближчої вершини
         for (int i = 0; i < graph->numCities; ++i) {
-            if (!visited[i] && (nearestVertex == -1 || graph->adjacency_matrix[currentVertex][i] < graph->adjacency_matrix[currentVertex][nearestVertex])) {
+            if (!visited[i] && (nearestVertex == -1 || graph->adjacency_matrix[currentVertex][i] < minDistance)) {
                 nearestVertex = i;
+                minDistance = graph->adjacency_matrix[currentVertex][i];
             }
         }
 
         visited[nearestVertex] = 1; // Позначаємо найближчу вершину як відвідану
         path[pathIndex++] = nearestVertex; // Додаємо найближчу вершину до шляху
+        totalDistance += minDistance; // Додаємо відстань до загальної відстані
         currentVertex = nearestVertex; // Переходимо до найближчої вершини
     }
 
+    printf("\n\n================\n");
+
     // 3. Виведення маршруту
-    printf("\nМаршрут, знайдений за допомогою алгоритму найближчого сусіда:\n");
+    printf("\n У результаті знайдений маршрут ");
     for (int i = 0; i < graph->numCities; ++i) {
-        printf("%d ", path[i]);
+        printf("%d-", path[i]);
     }
-    printf("\n");
+
+    // 4. Виведення загальної відстані
+    printf("\b, який становить відстань:\n ");
+    for (int i = 0; i < graph->numCities - 1; ++i) {
+        printf("%.2f + ", graph->adjacency_matrix[path[i]][path[i + 1]]);
+    }
+    printf("\b\b= %.2f кілометрів.\n\n================\n\n", totalDistance);
 }
